8dbae54a93d9d2f00b67b8d1e54873ae9465ecfa
Suchikant12 <suchi.bitu@gmail.com>
2023-12-14 13:00:06 +0530

    committed

---
+++ src/test/java/practice/Demo.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://www.google.co.in/");
+
+	}
+
+}
